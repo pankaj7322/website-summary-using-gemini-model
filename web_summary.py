701090af081ef1b8c9487c0f304b3e222229cd54
Pankaj <pankajkumar732298@gmail.com>
2024-05-07 18:05:57 +0530

    dropdown updated

--- web_summary.py
+++ web_summary.py
@@ -1,4 +1,3 @@
-# Importing necessary modules
 import os
 import google
 import google.generativeai as genai
@@ -10,107 +9,87 @@ from selenium.webdriver.chrome.options import Options
 import re
 import time
 
+# Function to add URL to session state dictionary
+def selection_box(url):
+    website_name = extract_name(url)
+    st.session_state.url_title_dict[website_name] = url
+    st.session_state.url_title_keys = list(st.session_state.url_title_dict.keys())
+    
+# Prompt text for generative model
+prompt_text = ''' 
+        Give the descriptive summary about the website
 
+        Generate the keywords related to it.
+'''
 
+# Initialize session state variables
+if 'url_title_dict' not in st.session_state:
+    st.session_state.url_title_dict = {}
 
+if 'url_title_keys' not in st.session_state:
+    st.session_state.url_title_keys = []
+
+# Function to extract visible text from website
 def extract_website_data(url):
-
-    # create chrome options
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    # WebDriver Chrome
     driver = webdriver.Chrome(options=chrome_options)
-
-    # Target URL
     driver.get(url)
-    # # To load entire webpage
-    time.sleep(1)
-
-    # Getting only the visible text on the website
+    time.sleep(5)
     visible_text = driver.find_element(By.TAG_NAME, "body").text
+    driver.close()
     return visible_text
 
-    # Closing the driver
-    driver.close()
+# Function to display summary and keywords
+def printing_summary(response_new):
+    output_placeholder = st.empty()
+    output_placeholder.empty()
+    summary_text, keywords_text = response_new.split("Keywords")
+    keywords_list = re.findall(r'\* (.+)', keywords_text)
+    text = ",  ".join(keywords_list)
+    st.write(text)
+    st.write(summary_text.strip())
 
+# Function to generate content using generative AI model
 def generativeai_model(prompt, visible_text):
-# accessing google api model
     api_key = "AIzaSyDfzdWQ9YonIv-5qkBXVO0jhS7bCMjd8xo"
     genai.configure(api_key = api_key)
-
     model = genai.GenerativeModel('gemini-pro')
-
-    # prompt = '''
-    #     create the summary from the given contents
-    # '''
-
     response = model.generate_content([prompt, visible_text])
     return response.text
 
-
-
-# extract name of the website
-
+# Function to extract website name from URL
 def extract_name(url):
     parsed_url = urlparse(url)
     website_name = parsed_url.netloc.split('.')[1]
     return website_name
 
-
-# create the list
-selectboxlist = []
-
-# create the ui
+# Main function to create the UI
 def main():
+    st.header(f'Website Summary')
 
-    # main document
-    st.header('Website Summary')
-    
-    # sidebar
+    # Sidebar input for URL
     url = st.sidebar.text_input("Enter the Website Link")
-    prompt_text = f''' 
-                    Give the summary about the website 
+    btn = st.sidebar.button("Submit")
 
-                    Generate the keywords related to it.
-                '''
-
-    # add the website name
-    
-
-    btn = st.sidebar.button("submit")
-    
-    # when button press
+    # When submit button is clicked
     if btn:
-        selectboxlist.append(extract_name(url))
-        st.sidebar.selectbox('select the website', selectboxlist)            
-
+        selection_box(url)
         visible_text = extract_website_data(url)
-        # display summary data
-
         response_new = generativeai_model(prompt_text, visible_text)
-        
-        summary_text, keywords_text = response_new.split("Keywords")
+        printing_summary(response_new)
 
-       
-
-        # Extracting the keywords from the keywords text
-        keywords_list = re.findall(r'\* (.+)', keywords_text)
-        # st.write(keywords_list)
-
-        # # Displaying the keywords
-        text = " , ".join(keywords_list)
-        st.write(text)
-
-        
-
-        
-
-        # Displaying the summary
-        st.write(summary_text.strip())
-
-        
-    
-
+    # Display selection box for saved websites
+    keys = st.session_state.url_title_keys
+    options = st.sidebar.selectbox("Select website", keys)
+    try:
+        if not btn:
+            url = st.session_state.url_title_dict[options]
+            visible_text = extract_website_data(url)
+            response_new = generativeai_model(prompt_text, visible_text)
+            printing_summary(response_new)
+    except Exception as e:
+        st.write("Enter the link")
 
 if __name__ == '__main__':
     main()
